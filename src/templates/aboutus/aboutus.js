dcedd7cc156c7eea8e8e4a2c02dd9f74613b59e1
Andry <_andry__@ukr.net>
2021-04-17 14:53:05 +0300

    add bootstrap 5 (inlcude only grid)

--- src/templates/aboutus/aboutus.js
+++ src/templates/aboutus/aboutus.js
@@ -1,4 +1,7 @@
-import './aboutus.scss';
+/* libs */
+import '../../libs/bootstrap-5.0.0-beta3/dist/css/bootstrap-grid.min.css';
+/* components */
 import '../../components/my_component/my_component.js';
+/* template */
+import './aboutus.scss';
 
-console.log('999');
